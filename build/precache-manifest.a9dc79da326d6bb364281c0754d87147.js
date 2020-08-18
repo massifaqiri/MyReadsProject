f4420dd8990dce530acd775845b306aee3efc6cc
faqima01 <faqima01@luther.edu>
2020-08-18 13:04:04 -0500

    Add firebase stuff

---
+++ build/precache-manifest.a9dc79da326d6bb364281c0754d87147.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1799bdbd70496d4a026",
+    "url": "/static/css/main.47d99655.chunk.css"
+  },
+  {
+    "revision": "e1799bdbd70496d4a026",
+    "url": "/static/js/main.e1799bdb.chunk.js"
+  },
+  {
+    "revision": "a16addfe54c956c2379b",
+    "url": "/static/js/1.a16addfe.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "9ae7b1cf4508181b31bee6d5411217a4",
+    "url": "/index.html"
+  }
+];
